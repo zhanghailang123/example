443a1727fd47fc1f652a79695c5c58c448e36106
hailang.zhang <hailang.zhang@zhaopin.com.cn>
2023-07-25 11:03:10 +0800

    feature：plugin test

---
+++ distribute-transaction/src/main/java/com/example/distributetransaction/DistributeTransactionApplication.java
@@ -0,0 +1,12 @@
+package com.example.distributetransaction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DistributeTransactionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DistributeTransactionApplication.class, args);
+    }
+}
