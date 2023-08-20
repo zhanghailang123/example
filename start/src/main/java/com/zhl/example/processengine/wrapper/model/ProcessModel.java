e0aebd6c7fd3e8783f653abe360bab33d754a67e
hailang.zhang <hailang.zhang@zhaopin.com.cn>
2023-08-20 13:49:28 +0800

    feature：流程引擎完善

--- start/src/main/java/com/zhl/example/processengine/wrapper/model/ProcessModel.java
+++ start/src/main/java/com/zhl/example/processengine/wrapper/model/ProcessModel.java
@@ -1,5 +1,9 @@
 package com.zhl.example.processengine.wrapper.model;
 
+import com.zhl.example.processengine.node.ProcessorDefinition;
+import com.zhl.example.processengine.node.ProcessorNode;
+import com.zhl.example.processengine.processor.Processor;
+import com.zhl.example.processengine.wrapper.instance.ProcessInstanceCreator;
 import lombok.Data;
 import org.springframework.context.annotation.Bean;
 
@@ -23,13 +27,61 @@ public class ProcessModel {
     }
 
     public void check() {
+        int startNode = 0;
         for (ProcessNodeModel processNodeModel : nodes.values()) {
             String className = processNodeModel.getClassName();
             try {
                 Class.forName(className);
-            } catch (Exception e) {
-
+            } catch (Throwable e) {
+                throw new IllegalArgumentException("无法加载节点[" + processNodeModel.getName() + "]的类：" + className);
+            }
+            String nextNode = processNodeModel.getNextNode();
+            if (nextNode != null) {
+                String[] nextNodes = nextNode.split(",");
+                for (String nodeName : nextNodes) {
+                    if (!nodes.containsKey(nodeName)) {
+                        throw new IllegalArgumentException("节点[name=" + nodeName + "]不存在");
+                    }
+                }
+            }
+            if (processNodeModel.getBegin()) {
+                startNode++;
             }
         }
+        if (startNode != 1) {
+            throw new IllegalArgumentException("不合法的流程，每个流程只能有一个开始节点");
+        }
+    }
+
+    public ProcessorDefinition build(ProcessInstanceCreator creator) throws Exception {
+        Map<String, ProcessorNode> processorNodeMap = new HashMap<>();
+        ProcessorDefinition processorDefinition = new ProcessorDefinition();
+        processorDefinition.setName(name);
+        // 第一次循环，将所有的processNode转化为processorNode实例，并保存在集合中，并将第一个节点放入processorDefinition
+        for (ProcessNodeModel processNodeModel : nodes.values()) {
+            String className = processNodeModel.getClassName();
+            Processor processor = creator.newInstance(className, processNodeModel.getName());
+            ProcessorNode processorNode = new ProcessorNode();
+            processorNode.setProcessor(processor);
+            processorNode.setName(processNodeModel.getName());
+            if (processNodeModel.getBegin()) {
+                processorDefinition.setFirst(processorNode);
+            }
+            processorNode.setInvokeMethod(processNodeModel.getInvokeMethod());
+            processorNodeMap.put(processNodeModel.getName(), processorNode);
+        }
+
+        // 第二次循环，将所有节点建立关联关系
+        for (ProcessorNode processNode : processorNodeMap.values()) {
+            String nextNodeStr = nodes.get(processNode.getName()).getNextNode();
+            if (nextNodeStr == null) {
+                continue;
+            }
+            String[] nextNodes = nextNodeStr.split(",");
+            for (String nextNode : nextNodes) {
+                processNode.addNextNode(processorNodeMap.get(nextNode));
+            }
+        }
+        return processorDefinition;
     }
 }
