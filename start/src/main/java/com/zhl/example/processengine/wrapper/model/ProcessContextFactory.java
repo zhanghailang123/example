4989a45ca16962a3bac678632270e9e22f446121
hailang.zhang <hailang.zhang@zhaopin.com.cn>
2023-08-31 09:31:06 +0800

    feature：流程引擎完善

--- start/src/main/java/com/zhl/example/processengine/wrapper/model/ProcessContextFactory.java
+++ start/src/main/java/com/zhl/example/processengine/wrapper/model/ProcessContextFactory.java
@@ -1,11 +1,14 @@
 package com.zhl.example.processengine.wrapper.model;
 
 import com.zhl.example.processengine.node.ProcessorDefinition;
+import com.zhl.example.processengine.processor.ProcessContext;
 import com.zhl.example.processengine.wrapper.instance.ProcessInstanceCreator;
 import com.zhl.example.processengine.wrapper.instance.ReflectNodeInstanceCreator;
 import lombok.extern.slf4j.Slf4j;
 
 import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 /**
  * @author hailang.zhang
@@ -17,6 +20,7 @@ public class ProcessContextFactory {
     private static final ProcessInstanceCreator DEFAULT_INSTANCE_CREATOR = new ReflectNodeInstanceCreator();
     private List<ProcessModel> modelList;
     private final ProcessInstanceCreator instanceCreator;
+    private final Map<String, ProcessorDefinition> processorDefinitionMap = new ConcurrentHashMap<>(16);
 
     public ProcessContextFactory(List<ProcessModel> modelList) throws Exception {
         this(modelList, DEFAULT_INSTANCE_CREATOR);
@@ -35,6 +39,16 @@ public class ProcessContextFactory {
 
         for (ProcessModel model : modelList) {
             ProcessorDefinition definition = model.build(instanceCreator);
+            log.info("流程构造成功");
+            processorDefinitionMap.put(definition.getName(), definition);
         }
+    }
+
+    public ProcessContext getContext(String name) {
+        ProcessorDefinition definition = processorDefinitionMap.get(name);
+        if (definition == null) {
+            throw new IllegalArgumentException("流程不存在");
+        }
+        return new ProcessContext(definition);
     }
 }
